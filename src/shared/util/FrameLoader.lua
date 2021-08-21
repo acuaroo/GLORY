7244497b261ed1b4cebee953c0af422570b4228e
Acuaro <67112172+acuaroo@users.noreply.github.com>
2021-08-21 11:12:36 -0600

    Added FrameLoader

---
+++ src/shared/util/FrameLoader.lua
@@ -0,0 +1,18 @@
+--|| VARS ||--
+local mainModule = {}
+
+--|| FUNCTIONS ||--
+function mainModule:ClearAllFrames(holder: GuiObject, storage: Folder, tween: boolean, tweenInfo: table)
+    if not tween then 
+        for _, frame: Frame in pairs(holder:GetChildren()) do
+            frame.Parent = storage
+        end
+    else
+        for _, frame: Frame in pairs(holder:GetChildren()) do
+            frame:TweenPosition(UDim2.new(0, 0, -1, 0), tweenInfo):Wait()
+            frame.Parent = storage
+        end
+    end
+
+    return true
+end
